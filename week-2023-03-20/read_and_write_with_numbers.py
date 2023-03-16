e4952856b9070d937219502075752cd7f01bac50
Livia Zaharia <liviazaharia2020@gmail.com>
2023-03-16 22:56:47 +0200

    done

---
+++ week-2023-03-20/read_and_write_with_numbers.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+
+def main():
+   file_name= input("What file are you searching for?")
+   
+   p=Path.cwd()
+   p0=p
+   p=p/file_name
+    
+   
+
+   if p in p0.glob("*"):
+    
+        
+        with open(p,"r") as file_obj:
+           lines_raw=file_obj.read()
+        
+        text_lines=lines_raw.split("\n")
+
+        new_name=input("new file name")+".txt"
+        p2=p0/new_name
+
+        file_obj2=open(p2,"x")
+        file_obj2.close()
+
+        with open(p2,"w",) as file_obj2:
+           for index,line in enumerate(text_lines):
+            file_obj2.write(f"{index}, {line}'\n'")
+          
+
+
+       
+
+if __name__=="__main__":
+    main()
